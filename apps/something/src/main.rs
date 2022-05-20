6787a576b3044894ed4e9c76d775be66f35d315f
Finn <34718806+FinnDore@users.noreply.github.com>
2022-05-20 22:25:21 +0100

    chore: added a few more nx configs

--- apps/something/src/main.rs
+++ apps/something/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!1243");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
