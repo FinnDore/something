d6736368f33bf7d8eb77bb7aaaea8b88992465a1
Finn <34718806+FinnDore@users.noreply.github.com>
2022-05-20 14:24:23 +0100

    chore(something-shop): updated the angular project

--- apps/something-shop/jest.config.js
+++ apps/something-shop/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    displayName: 'something',
+    displayName: 'something-shop',
     preset: '../../jest.preset.js',
     setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
     globals: {
@@ -8,7 +8,7 @@ module.exports = {
             stringifyContentPathRegex: '\\.(html|svg)$'
         }
     },
-    coverageDirectory: '../../coverage/apps/something',
+    coverageDirectory: '../../coverage/apps/something-shop',
     transform: {
         '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular'
     },
