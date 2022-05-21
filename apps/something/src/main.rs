bb05ee792213bae3eef4b034e2656c5b19983674
Finn <34718806+FinnDore@users.noreply.github.com>
2022-05-21 20:50:31 +0100

    chore: added base project for the frontend and api (#1)
    * chore: created project from starter
    
    * chore(something-shop): updated the angular project
    
    * build: added nx cloud
    
    * chore: added rust project
    
    * chore: added a few more nx configs
    
    * chore: updated the rust project config to enable caching properly
    
    * chore: fixed serve settings for something
    
    * build: added nx agents
    
    * chore(something): updated base route
    
    * build(something): added a version target
    
    * chore(something): added version logging
    
    * build(something): fixed rust-verision comand typo

---
+++ apps/something/src/main.rs
@@ -0,0 +1,15 @@
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!1"
+}
+
+#[launch]
+fn rocket() -> _ {
+    const VERSION: Option<&str> = option_env!("CARGO_PKG_VERSION");
+    println!("💸 something version {} 💸", VERSION.unwrap_or("UNKNOWN"));
+
+    rocket::build().mount("/api", routes![index])
+}
