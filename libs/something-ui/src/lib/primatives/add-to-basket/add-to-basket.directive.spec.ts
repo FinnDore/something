cc97a24f398c3d5c3d6c7e68f4bf242d9e54165e
Finn <34718806+FinnDore@users.noreply.github.com>
2022-06-10 20:20:10 +0100

    test(something-ui): added tests for the add-to-basket directive and shop store

--- libs/something-ui/src/lib/primatives/add-to-basket/add-to-basket.directive.spec.ts
+++ libs/something-ui/src/lib/primatives/add-to-basket/add-to-basket.directive.spec.ts
@@ -1,8 +1,83 @@
-// import { AddToBasketDirective } from './add-to-basket.directive';
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { BasketStore } from '../../stores/basket-store/basket.store';
+import { ItemStore } from '../../stores/item-store/item.store';
+import { ShopStore } from '../../stores/shop-store/shop.store';
+import { AddToBasketDirective } from './add-to-basket.directive';
 
-// describe('AddToBasketDirective', () => {
-//     it('should create an instance', () => {
-//         const directive = new AddToBasketDirective();
-//         expect(directive).toBeTruthy();
-//     });
-// });
+@Component({
+    selector: 's-test-component',
+    template: '<button s-add-to-basket></button>'
+})
+class TestComponent {}
+
+describe('AddToBasketDirective', () => {
+    let shopStore: ShopStore;
+    let basketStoreSpy: jest.SpyInstance;
+    let fixture: ComponentFixture<TestComponent>;
+    let itemStore: ItemStore;
+    let button: HTMLButtonElement;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [TestComponent, AddToBasketDirective],
+            providers: [ShopStore, BasketStore, ItemStore]
+        }).compileComponents();
+        fixture = TestBed.createComponent(TestComponent);
+        shopStore = TestBed.inject(ShopStore);
+        itemStore = TestBed.inject(ItemStore);
+        const basketStore = TestBed.inject(BasketStore);
+        basketStoreSpy = jest.spyOn(basketStore, 'addItem');
+
+        button = fixture.debugElement.query(By.css('button')).nativeElement;
+
+        shopStore.patchState({
+            items: [
+                {
+                    itemId: '1'
+                },
+                {
+                    itemId: '2'
+                }
+            ]
+        });
+    });
+
+    it('should add the current item to the basket', () => {
+        itemStore.patchState({
+            selectedItemId: '1'
+        });
+
+        button.click();
+
+        expect(basketStoreSpy).toHaveBeenCalledTimes(1);
+        expect(basketStoreSpy).toBeCalledWith('1');
+    });
+
+    it('should add the current item to the basket after it has changed', () => {
+        itemStore.patchState({
+            selectedItemId: '1'
+        });
+        button.click();
+
+        expect(basketStoreSpy).toHaveBeenCalledTimes(1);
+        expect(basketStoreSpy).toBeCalledWith('1');
+
+        itemStore.patchState({
+            selectedItemId: '2'
+        });
+        button.click();
+
+        expect(basketStoreSpy).toHaveBeenCalledTimes(2);
+        expect(basketStoreSpy).toBeCalledWith('2');
+    });
+
+    it('should not add and item if the item is null', () => {
+        itemStore.patchState({
+            selectedItemId: null as unknown as string
+        });
+        button.click();
+        expect(basketStoreSpy).not.toHaveBeenCalled();
+    });
+});
--- libs/something-ui/src/lib/stores/shop-store/shop.store.spec.ts
+++ libs/something-ui/src/lib/stores/shop-store/shop.store.spec.ts
@@ -1,32 +1,34 @@
-// import { TestBed } from '@angular/core/testing';
-// import { TestScheduler } from 'rxjs/testing';
-// import { Item } from '../models/item.model';
-// import { UtilsasketStore } from './shop.store';
+import { TestBed } from '@angular/core/testing';
+import { TestScheduler } from 'rxjs/testing';
+import { Item } from '../../models/item.model';
+import { ShopStore } from './shop.store';
 
-// const testScheduler = new TestScheduler((actual, expected) =>
-//     expect(actual).toEqual(expected)
-// );
+const testScheduler = new TestScheduler((actual, expected) =>
+    expect(actual).toEqual(expected)
+);
 
-// describe('UtilsasketStoreService', () => {
-//     let basketStore: UtilsasketStore;
+describe('ShopStore', () => {
+    let shopStore: ShopStore;
 
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({
-//             providers: [UtilsasketStore]
-//         });
-//         basketStore = TestBed.inject(UtilsasketStore);
-//     });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [ShopStore]
+        });
+        shopStore = TestBed.inject(ShopStore);
+    });
 
-//     it('should return the given item with the given id', () => {
-//         testScheduler.run(({ expectObservable }) => {
-//             const item: Item = {
-//                 id: '1'
-//             };
-//             basketStore.patchState({
-//                 items: [item]
-//             });
+    it('should return the given item with the given id', () => {
+        testScheduler.run(({ expectObservable }) => {
+            const item: Item = {
+                itemId: '1'
+            };
+            shopStore.patchState({
+                items: [item]
+            });
 
-//             expectObservable(basketStore.getItem('1')).toBe('a', { a: item });
-//         });
-//     });
-// });
+            expectObservable(shopStore.getItem('1'), '-!').toBe('a', {
+                a: item
+            });
+        });
+    });
+});
