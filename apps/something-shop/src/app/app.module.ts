e2bd5a1d60bc6b02284f69ee1655d0c2e63c78a1
Finn <34718806+FinnDore@users.noreply.github.com>
2022-06-12 15:25:05 +0100

    build: re added @ngrx/component

--- apps/something-shop/src/app/app.module.ts
+++ apps/something-shop/src/app/app.module.ts
@@ -3,8 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { TuiRootModule } from '@taiga-ui/core';
 import { AppComponent } from './app.component';
-
-import { ShopStore } from "@something/s-ui/cdk/stores/shop-store"
+import { ShopStore } from '@something/s-ui/cdk/stores/shop-store';
 
 @NgModule({
     declarations: [AppComponent],
