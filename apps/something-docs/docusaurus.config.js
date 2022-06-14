ba519a350aa546594e35436eb13334294b5fd9bf
Finn <34718806+FinnDore@users.noreply.github.com>
2022-06-14 17:39:02 +0100

    docs(something-docs): some minor tweaks

--- apps/something-docs/docusaurus.config.js
+++ apps/something-docs/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
     title: 'Something',
-    tagline: 'Dinosaurs are cool',
+    tagline: 'Bring the UI and we will bring the shop',
     url: 'https://your-docusaurus-test-site.com',
     baseUrl: '/',
     onBrokenLinks: 'throw',
@@ -15,7 +15,6 @@ const config = {
     favicon: 'img/favicon.ico',
     organizationName: 'FinnDore', // Usually your GitHub org/user name.
     projectName: 'Something', // Usually your repo name.
-
     presets: [
         [
             'classic',
@@ -24,14 +23,12 @@ const config = {
                 docs: {
                     sidebarPath: require.resolve('./sidebars.js'),
                     // Please change this to your repo.
-                    editUrl:
-                        'https://github.com/facebook/docusaurus/edit/main/website/'
+                    editUrl: 'https://github.com/FinnDore/something'
                 },
                 blog: {
                     showReadingTime: true,
                     // Please change this to your repo.
-                    editUrl:
-                        'https://github.com/facebook/docusaurus/edit/main/website/blog/'
+                    editUrl: 'https://github.com/FinnDore/something'
                 },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css')
@@ -52,14 +49,12 @@ const config = {
                 items: [
                     {
                         type: 'doc',
-                        docId: 'docs',
+                        docId: 'getting-started/cdk',
                         position: 'left',
                         label: 'Docs'
                     },
                     { to: '/blog', label: 'Blog', position: 'left' },
-                    {
-                        type: 'docsVersionDropdown'
-                    },
+
                     {
                         href: 'https://github.com/FinnDore/something',
                         label: 'GitHub',
@@ -74,23 +69,7 @@ const config = {
                         title: 'Docs',
                         items: []
                     },
-                    {
-                        title: 'Community',
-                        items: [
-                            {
-                                label: 'Stack Overflow',
-                                href: 'https://stackoverflow.com/questions/tagged/docusaurus'
-                            },
-                            {
-                                label: 'Discord',
-                                href: 'https://discordapp.com/invite/docusaurus'
-                            },
-                            {
-                                label: 'Twitter',
-                                href: 'https://twitter.com/docusaurus'
-                            }
-                        ]
-                    },
+
                     {
                         title: 'More',
                         items: [
@@ -100,12 +79,12 @@ const config = {
                             },
                             {
                                 label: 'GitHub',
-                                href: 'https://github.com/facebook/docusaurus'
+                                href: 'https://github.com/FinnDore/something'
                             }
                         ]
                     }
                 ],
-                copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`
+                copyright: `Copyright © ${new Date().getFullYear()} Something, Inc. Built with Docusaurus.`
             },
             prism: {
                 theme: lightCodeTheme,
--- apps/something-docs/src/pages/index.tsx
+++ apps/something-docs/src/pages/index.tsx
@@ -17,10 +17,9 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-    const { siteConfig } = useDocusaurusContext();
     return (
         <Layout
-            title={`Hello from ${siteConfig.title}`}
+            title={`Docs`}
             description="Description will go into a meta tag in <head />"
         >
             <HomepageHeader />
