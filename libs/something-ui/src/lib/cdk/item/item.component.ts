144f7e28a991c05fe039f923d043c4b7e797af64
Finn <34718806+FinnDore@users.noreply.github.com>
2022-06-15 20:36:21 +0100

    chore(something-ui): removed some unused imports

--- libs/something-ui/src/lib/cdk/item/item.component.ts
+++ libs/something-ui/src/lib/cdk/item/item.component.ts
@@ -5,19 +5,15 @@ import {
     Input,
     NgModule
 } from '@angular/core';
-import { LetModule, ReactiveComponentModule } from '@ngrx/component';
-import { loadStripe, Stripe } from '@stripe/stripe-js';
 import { TuiButtonModule } from '@taiga-ui/core';
-import { take } from 'rxjs';
-import { ItemStore } from '../stores/item-store/item.store';
 import { AddToBasketDirectiveModule } from '../primitives/add-to-basket/add-to-basket.directive';
+import { CheckoutDirectiveModule } from '../primitives/checkout/checkout.directive';
 import { ItemDirectiveModule } from '../primitives/item/item.directive';
+import { RemoveFromBasketModule } from '../primitives/remove-from-basket';
 import { BasketStore } from '../stores/basket-store/basket.store';
+import { ItemStore } from '../stores/item-store/item.store';
 import { ShopStore } from '../stores/shop-store/shop.store';
 import { Unsubscribe, UnsubscribeModule } from '../utils/angular/unsubscribe';
-import { log } from '../utils/rx/log';
-import { RemoveFromBasketModule } from '../primitives/remove-from-basket';
-import { CheckoutDirectiveModule } from '../primitives/checkout/checkout.directive';
 
 @Component({
     selector: 's-example-item',
