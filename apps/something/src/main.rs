368915695ecd27b0a8b4e8d7e0a0c5fb42cd9a6c
Finn <34718806+FinnDore@users.noreply.github.com>
2022-07-21 17:28:32 +0100

    chore: some debug changes

--- apps/something/src/main.rs
+++ apps/something/src/main.rs
@@ -12,8 +12,8 @@ extern crate rocket;
 extern crate diesel;
 
 use diesel::{mysql::MysqlConnection, Connection};
-use rocket::tokio;
-use std::env;
+use rocket::{tokio, Config};
+use std::{env, net::Ipv4Addr};
 
 pub fn establish_connection() -> MysqlConnection {
     let database_url =
@@ -30,9 +30,14 @@ fn rocket() -> _ {
     env::set_var("ROCKET_ADDRESS", "0.0.0.0");
     const A: Option<&str> = option_env!("ROCKET_ADDRESS");
 
+    let mut config = Config::debug_default();
+    config.address = Ipv4Addr::new(0, 0, 0, 0).into();
+    config.port = 80;
+    // port: ,
+    // address: ,
     println!("💸 something version {} 💸", VERSION.unwrap_or("UNKNOWN"));
     println!("addr {} 💸", ADDR.unwrap_or("UNKNOWN"));
     println!("a {} 💸", A.unwrap_or("UNKNOWN"));
 
-    rocket::build().mount("/", routes![checkout, add_item])
+    rocket::custom(config).mount("/", routes![checkout, add_item])
 }
