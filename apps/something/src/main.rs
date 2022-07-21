a4a1f558d189dea3700c046fa4d48899f9ccb43f
Finn <34718806+FinnDore@users.noreply.github.com>
2022-07-21 20:06:24 +0100

    fix: added rocket config

--- apps/something/src/main.rs
+++ apps/something/src/main.rs
@@ -12,8 +12,6 @@ extern crate rocket;
 extern crate diesel;
 
 use diesel::{mysql::MysqlConnection, Connection};
-use rocket::Config;
-use std::{env, net::Ipv4Addr};
 
 pub fn establish_connection() -> MysqlConnection {
     let database_url =
@@ -26,18 +24,9 @@ pub fn establish_connection() -> MysqlConnection {
 fn rocket() -> _ {
     const VERSION: Option<&str> = option_env!("CARGO_PKG_VERSION");
     const ADDR: Option<&str> = option_env!("ROCKET_ADDRESS");
-    // check if we are running a release build or not
-    env::set_var("ROCKET_ADDRESS", "0.0.0.0");
-    const A: Option<&str> = option_env!("ROCKET_ADDRESS");
 
-    let mut config = Config::debug_default();
-    config.address = Ipv4Addr::new(0, 0, 0, 0).into();
-    config.port = 80;
-    // port: ,
-    // address: ,
     println!("💸 something version {} 💸", VERSION.unwrap_or("UNKNOWN"));
-    println!("addr {} 💸", ADDR.unwrap_or("UNKNOWN"));
-    println!("a {} 💸", A.unwrap_or("UNKNOWN"));
+    println!("Rocket address is {}", ADDR.unwrap_or("UNKNOWN"));
 
-    rocket::custom(config).mount("/", routes![checkout, add_item])
+    rocket::build().mount("/", routes![checkout, add_item])
 }
