52a2ed1f3e8bb56cb562feef91eb0ab648e45222
Finn <34718806+FinnDore@users.noreply.github.com>
2022-07-21 13:58:42 +0100

    chore(something): removed diesel stuff for testing

--- apps/something/src/main.rs
+++ apps/something/src/main.rs
@@ -1,40 +1,31 @@
-mod api;
-mod enums;
-pub mod models;
-pub mod schema;
-use api::add_item::add_item;
-use api::checkout::checkout;
+// mod api;
+// mod enums;
+// pub mod models;
+// pub mod schema;
+// use api::add_item::add_item;
+// use api::checkout::checkout;
 extern crate dotenv;
 
 #[macro_use]
 extern crate rocket;
-#[macro_use]
-extern crate diesel;
+// #[macro_use]
+// extern crate diesel;
 
-use diesel::{mysql::MysqlConnection, Connection};
-use rocket::tokio;
+// use diesel::{mysql::MysqlConnection, Connection};
 use std::env;
 
-pub fn establish_connection() -> MysqlConnection {
-    let database_url =
-        env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    MysqlConnection::establish(&database_url)
-        .expect("Error connecting to database")
-}
+// pub fn establish_connection() -> MysqlConnection {
+//     let database_url =
+//         env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+//     MysqlConnection::establish(&database_url)
+//         .expect("Error connecting to database")
+// }
 
-#[tokio::main]
-async fn main() {
+#[launch]
+fn rocket() -> _ {
     const VERSION: Option<&str> = option_env!("CARGO_PKG_VERSION");
 
     println!("💸 something version {} 💸", VERSION.unwrap_or("UNKNOWN"));
 
-    if let Err(err) = rocket::build()
-        .mount("/", routes![checkout, add_item])
-        .launch()
-        .await
-    {
-        println!("{}", err);
-    }
-
-    println!("aaaaaaaaaa");
+    rocket::build().mount("/", routes![])
 }
