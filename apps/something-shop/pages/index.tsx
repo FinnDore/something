a0a9b68f5ed931efbc08fd87b06f5d9c6ffe06b8
Finn <34718806+FinnDore@users.noreply.github.com>
2022-07-22 14:26:48 +0100

    chore(something-shop): added color selection

--- apps/something-shop/pages/index.tsx
+++ apps/something-shop/pages/index.tsx
@@ -1,12 +1,20 @@
+import { useState } from 'react';
+
 const ItemColors: React.FC<{
     colors: string[];
-}> = ({ colors }) => {
+    selected?: string;
+    onSelect: (color: string) => void;
+}> = ({ colors, selected, onSelect }) => {
+    const selectedClass = 'border';
     return (
         <div className="-ml-1">
             {colors.map(color => (
                 <button
                     key={color}
-                    className="m-1 h-5 w-5 rounded-md border-[.5px] border-zinc-200"
+                    onClick={() => onSelect(color)}
+                    className={`m-1 h-5 w-5 rounded-md border-[.5px] border-zinc-200 ${
+                        color === selected ? selectedClass : ''
+                    } : ''`}
                     style={{ backgroundColor: color }}
                 ></button>
             ))}
@@ -17,24 +25,34 @@ const ItemColors: React.FC<{
 const Item: React.FC<{
     name: string;
     price: string;
-    colors: string[];
+    colors?: string[];
 }> = ({ name, price, colors }) => {
-    // function to add an item to the basket
+    const [selectedColor, setSelectedColor] = useState(colors?.[0]);
 
     return (
-        <div className="border-color-white dark:border-color-white flex h-[24rem] w-72 flex-col rounded-md border-[.5px] px-3 py-5">
+        <div className="border-color-white dark:border-color-white flex h-[24rem] w-72 flex-col rounded-md border-[.5px] px-3 py-5 drop-shadow-2xl">
             <div className="grid h-full place-items-center">
-                <div className="h-40 w-40 rounded-lg bg-black dark:border dark:border-red-600 dark:bg-white"></div>
+                <div
+                    className="h-40 w-40 rounded-lg border drop-shadow-lg transition-colors"
+                    style={{
+                        backgroundColor: selectedColor,
+                        borderColor: selectedColor
+                    }}
+                ></div>
             </div>
             <div className="w-full px-2">
                 <div className="mb-4 w-full border-[.5px] dark:border-white"></div>
                 <div className="flex">
                     <div>
                         <h1 className="text-lg">{name}</h1>
-                        <ItemColors colors={colors} />
+                        <ItemColors
+                            colors={colors}
+                            selected={selectedColor}
+                            onSelect={setSelectedColor}
+                        />
                         <div>{price}</div>
                     </div>
-                    <button className="ml-auto mt-auto h-min rounded-md border-[.5px] border-transparent bg-gradient-to-r from-amber-500 to-pink-600 px-5 py-1 transition-colors hover:border-white">
+                    <button className="ml-auto mt-auto h-min rounded-md border border-transparent bg-gradient-to-r from-purple-600 to-pink-600 px-5 py-1 transition-colors hover:border-black hover:dark:border-white">
                         BUY
                     </button>
                 </div>
@@ -45,11 +63,11 @@ const Item: React.FC<{
 
 export function Index() {
     return (
-        <div className="grid h-screen w-screen place-items-center bg-white text-white dark:bg-black">
+        <div className="grid h-screen w-screen place-items-center bg-white dark:bg-black dark:text-white">
             <Item
-                name="White Square"
+                name="Just a Square"
                 price="£20"
-                colors={['red', 'aqua', 'yellow']}
+                colors={['#32a87b', '#7132a8', '#a88932']}
             />
         </div>
     );
